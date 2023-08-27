3f76a1f1bb9a04a7e90e55055285ec818203c953
Rupesh Raut <rupeshraut99396@gmail.com>
2023-08-28 05:18:21 +0530

    bug fix

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
 export default function Home() {
+  const Router = useRouter()
+  useEffect(() => {
+    Router.push("/MyFiles")
+  }, [])
+
   return (
     <>
       Hello World!
